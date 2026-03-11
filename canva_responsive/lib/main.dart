945275ee31141d92dc43a72e35d032fce31b562f
pongpipha-k <pongpiphat_k@cmu.ac.th>
2026-03-11 16:56:41 +0700

    setup theme and front

--- canva_responsive/lib/main.dart
+++ canva_responsive/lib/main.dart
@@ -1,36 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CanvaResponsiveApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CanvaResponsiveApp extends StatelessWidget {
+  const CanvaResponsiveApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Canva Responsive',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'SF Pro Display',
+        scaffoldBackgroundColor: const Color(0xFF05060A),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF8A63F5),
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CanvaHomeScreen(),
     );
   }
 }
